bb0046412300710cb958add482e5df83c8df73e1
Godfrey Nolan <godfrey@riis.com>
2015-11-09 07:55:57 -0500

    Added 2nd feature

---
+++ Chapter6/Zodiac/feature2/app/src/main/java/com/riis/zodiac/MainActivity.java
@@ -0,0 +1,28 @@
+package com.riis.zodiac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private Zodiac zodiac;
+    private TextView mtxtSelectedItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        zodiac = new Zodiac();
+        List<String> list = Arrays.asList(zodiac.getAllStarSign());
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+        ListView listView = (ListView) findViewById(R.id.list_of_signs);
+        listView.setAdapter(adapter);
+    }
+
+}
---
+++ Chapter6/Zodiac/feature2/app/src/test/java/com/riis/zodiac/ZodiacUnitTest.java
@@ -0,0 +1,31 @@
+package com.riis.zodiac;
+
+import android.widget.ListView;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ZodiacUnitTest
+{
+    private final int ARIES_ID  = 0;
+    private Zodiac tZodiac;
+
+    @Before
+    public void setUp() {
+        tZodiac = new Zodiac();
+        //mainActivity = Robolectric.setupActivity(MainActivity.class);
+        //assertNotNull("Mainactivity not instantiated", mainActivity);
+        //lstView = (ListView) mainActivity.findViewById(R.id.list_of_signs);//getting the list layout xml
+    }
+
+    @Test
+    public void firstStarSign_isAries() throws Exception {
+        String sSign = tZodiac.getStarSign(ARIES_ID);
+        assertEquals("Aries", sSign);
+    }
+}
